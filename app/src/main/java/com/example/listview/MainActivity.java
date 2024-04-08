49903e1e9e3685eda0627298996abe2b7786f7f3
kanmanikannu <114866367+kanmanikannu@users.noreply.github.com>
2024-04-08 09:23:32 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,14 +1,20 @@
-package com.example.listview;
+Main activity: package com.example.list_view;
+
+import android.os.Bundle; import android.widget.ArrayAdapter; import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+ListView simpleList;
+String countryList[] = {"India", "China", "australia", "Portugal", "America", "NewZealand"};
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    simpleList = (ListView)findViewById(R.id.simpleListView);
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+    simpleList.setAdapter(arrayAdapter);
+}
 }
